8ab3752e31a66fdd7d52dd40cc3cf5afa9ae7f0d
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-19 17:07:55 -0400

    TeamDetailPage, Bind dynamic team name in page title - Dump navParam team data in page

--- src/pages/team-detail/team-detail.ts
+++ src/pages/team-detail/team-detail.ts
@@ -1,20 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the TeamDetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @Component({
   selector: 'page-team-detail',
   templateUrl: 'team-detail.html',
 })
 export class TeamDetailPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	public team:any = {};
+
+  constructor(
+	  public navCtrl: NavController
+	  , public navParams: NavParams
+	) {
+	this.team = this.navParams.data;//will be an incoming Team instance
+	console.log("navParams:", this.navParams );
   }
 
   ionViewDidLoad() {
