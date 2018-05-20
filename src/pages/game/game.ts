8bc30fb232091af40ac1069d20afb69e0bf50ea8
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-20 14:05:36 -0400

    GameParge, Show both Teams in a Game & btn to visit TeamDetails - Add onClickTeam() to navigate to TeamDetails for the chosen team

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,12 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { EliteApi } from '../../providers/elite-api/elite-api';
+import { TeamHomePage } from '../team-home/team-home';
 
-/**
- * Generated class for the GamePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @Component({
   selector: 'page-game',
@@ -14,11 +10,25 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class GamePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	public game: any	= {};
+
+  constructor(
+	  public navCtrl: NavController
+	  ,public navParams: NavParams
+	  ,private eliteApi: EliteApi
+	) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad GamePage');
+	this.game = this.navParams.data;
+  }
+
+  onClickTeam( teamId ){
+	let tour	= this.eliteApi.getCurrentTour()
+	,team		= tour.teams.find( t => teamId === t.id )
+	;
+	this.navCtrl.push( TeamHomePage, team );
   }
 
 }
