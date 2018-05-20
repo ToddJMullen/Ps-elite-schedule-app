fb22f7f93f32d10e3047006eb9ad5434373633f6
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-20 13:58:09 -0400

    TournamentsPage, Use EliteApi to retrieve the list of tournaments

--- src/pages/tournaments/tournaments.ts
+++ src/pages/tournaments/tournaments.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { TeamsPage } from '../teams/teams';
 import { EliteApi } from '../../providers/elite-api/elite-api';
 
@@ -15,16 +15,28 @@ export class TournamentsPage {
 	  public navCtrl: NavController
 	  , public navParams: NavParams
 	  , public eliteApi: EliteApi
+	  , public loadingController: LoadingController
 	) {
   }
 
   ionViewDidLoad() {
 	console.log('ionViewDidLoad LOADED TournamentsPage (first time loaded)');
-	this.eliteApi.getTournaments().then( tourAry => this.tournamentAry = tourAry );
+	let loader = this.loadingController.create({
+		content: "Getting Tournaments..."
+		// ,spinner: "dots"
+	});
+
+	loader.present().then( () => {
+		//using a Promise
+		this.eliteApi.getTournaments().then( tourAry => {
+			this.tournamentAry = tourAry;
+			loader.dismiss();
+		});
+	})
   }
 
-  itemTapped( $event, tourney ){
-	this.navCtrl.push( TeamsPage, tourney );
+  itemTapped( $event, tour ){
+	this.navCtrl.push( TeamsPage, tour );
   }
 
 
