d44d7e773e38de0bd385fa9202b8de6a0862ea0f
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-19 22:55:05 -0400

    EliteApi, implement getTournaments()

--- src/providers/elite-api/elite-api.ts
+++ src/providers/elite-api/elite-api.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { Http } from '@angular/http';//includes json()
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -6,10 +7,17 @@ export class EliteApi {
 
 	private baseUri:string = "https://elite-schedule-app-i3-37c3e.firebaseio.com";
 
-  constructor(public http: HttpClient) {
+  constructor(
+	  public http: Http
+	) {
 	// console.log('Hello EliteApiProvider Provider');
+  }
 
-
+  getTournaments(){
+	  return new Promise( resolve => {
+		  this.http.get(`${this.baseUri}/tournaments.json`)
+				.subscribe( rsp => resolve( rsp.json() ) );
+	  });
   }
 
 }
