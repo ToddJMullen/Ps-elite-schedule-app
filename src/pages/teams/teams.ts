7f7cefca514bea8c8a62114af020d37872573f1c
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-20 15:17:24 -0400

    TeamsPage, morph tournament data to group all division teams by their division

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { TeamHomePage } from '../team-home/team-home';
 import { EliteApi } from '../../providers/elite-api/elite-api';
+import * as _ from "lodash";
 
 @Component({
   selector: 'page-team',
@@ -10,20 +11,36 @@ import { EliteApi } from '../../providers/elite-api/elite-api';
 export class TeamsPage {
 
 	public teams = [];
+	private allTeams:any;
+	private allTeamDivisions:any;
 
   constructor(
 	  public navCtrl: NavController
 	  , public navParams: NavParams
 	  , public eliteApi: EliteApi
+	  ,private loadingController: LoadingController
 	) {
   }
 
   ionViewDidLoad() {
 	console.log('ionViewDidLoad TeamPage');
 	let selectedTour = this.navParams.data;
-	//using rxjs map
-	this.eliteApi.getTournamentData( selectedTour.id ).subscribe( tournament => {
-		this.teams = tournament.teams;
+	let loader = this.loadingController.create({
+		content: "Loading Data..."
+	});
+	loader.present().then(() => {
+		//using rxjs map
+		this.eliteApi.getTournamentData( selectedTour.id ).subscribe( tournament => {
+			this.allTeams = tournament.teams;
+			this.allTeamDivisions = _.chain( tournament.teams )
+			.groupBy("division")
+			.toPairs()
+			.map( team => _.zipObject(["divisionName", "divisionTeams"], team) )
+			.value();
+			this.teams = this.allTeamDivisions;
+			console.log("All teams division:", this.allTeamDivisions );
+		});
+		loader.dismiss();
 	});
   }
 
