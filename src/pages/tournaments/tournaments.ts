9c0c7575d52ef8bccbc306b8bc364f36ff5a26f9
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-19 23:31:35 -0400

    Tournaments, Get al tournaments via EliteApi & list buttons dynamically

--- src/pages/tournaments/tournaments.ts
+++ src/pages/tournaments/tournaments.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { TeamsPage } from '../teams/teams';
+import { EliteApi } from '../../providers/elite-api/elite-api';
 
 @Component({
   selector: 'page-tournaments',
@@ -8,15 +9,48 @@ import { TeamsPage } from '../teams/teams';
 })
 export class TournamentsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+	tournamentAry: any;
+
+  constructor(
+	  public navCtrl: NavController
+	  , public navParams: NavParams
+	  , public eliteApi: EliteApi
+	) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TournamentsPage');
+	console.log('ionViewDidLoad LOADED TournamentsPage (first time loaded)');
+	this.eliteApi.getTournaments().then( tourAry => this.tournamentAry = tourAry );
   }
 
-  itemTapped(){
-	this.navCtrl.push( TeamsPage );
+  itemTapped( $event, tourney ){
+	this.navCtrl.push( TeamsPage, tourney );
   }
+
+
+//   ionViewWillEnter(){
+//     console.log('ionViewWillEnter just BEFORE ENTERING TournamentsPage');
+//   }
+
+//   ionViewWillLeave(){
+//     console.log('ionViewWillLeave just BEFORE leaving TournamentsPage');
+//   }
+
+//   ionViewDidLeave(){
+//     console.log('ionViewDidLeave just after leaving TournamentsPage');
+//   }
+
+//   ionViewWillUnload(){
+//     console.log('ionViewWillUnload just BEFORE UNLOAD TournamentsPage');
+//   }
+
+//   ionViewCanEnter(){
+//     console.log('ionViewCanEnter run/guard just BEFORE LOADING TournamentsPage (check auth etc.)');
+//   }
+
+//   ionViewCanLeave(){
+//     console.log('ionViewCanLeave run/guard just BEFORE UNLOADING TournamentsPage (check forms/changes etc)');
+//   }
+
 
 }
