91e9fbc41820e70e6b448cc2038d3cc7185f513b
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-20 17:04:12 -0400

    StandingsPage, Add Team . Division standing in column based layout

--- src/pages/standings/standings.ts
+++ src/pages/standings/standings.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { EliteApi } from '../../providers/elite-api/elite-api';
+import * as _ from "lodash";
 
 @Component({
   selector: 'page-standings',
@@ -7,18 +9,32 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class StandingsPage {
 
+
+	public allStandings: any[];
+	public standings: any[];
+
 	public team:any		= {};
 
   constructor(
 	  public navCtrl: NavController
 	  , public navParams: NavParams
+	  , private eliteApi: EliteApi
 	) {
 		console.log("navParams:", this.navParams );
-		this.team = this.navParams.data;
-  }
+	}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad StandingsPage');
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad StandingsPage');
+		this.team = this.navParams.data;
+		let tour = this.eliteApi.getCurrentTour();
+		this.standings = tour.standings;
+		this.allStandings = _.chain( this.standings )
+								.groupBy("division")
+								.toPairs()
+								.map( game => _.zipObject(["divisionName","divisionStandings"], game ))
+								.value();
+		console.log("Standings:", this.standings );
+		console.log("All Standings:", this.allStandings );
   }
 
 }
