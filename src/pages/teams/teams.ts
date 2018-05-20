76fd6bab366f3b8d4b3894967dd1d5b235f0c8d9
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-20 13:57:11 -0400

    TeamsPage, Replace static team data with call to fetch Tournament data

--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { TeamHomePage } from '../team-home/team-home';
+import { EliteApi } from '../../providers/elite-api/elite-api';
 
 @Component({
   selector: 'page-team',
@@ -8,20 +9,22 @@ import { TeamHomePage } from '../team-home/team-home';
 })
 export class TeamsPage {
 
-	public teams = [
-		{ id: 1, name: "HC Elite"}
-		,{ id: 2, name: "Team Takeover"}
-		,{ id: 3, name: "DC Thunder"}
-	]
+	public teams = [];
 
   constructor(
 	  public navCtrl: NavController
 	  , public navParams: NavParams
+	  , public eliteApi: EliteApi
 	) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TeamPage');
+	console.log('ionViewDidLoad TeamPage');
+	let selectedTour = this.navParams.data;
+	//using rxjs map
+	this.eliteApi.getTournamentData( selectedTour.id ).subscribe( tournament => {
+		this.teams = tournament.teams;
+	});
   }
 
   itemTapped( $event, team ){
