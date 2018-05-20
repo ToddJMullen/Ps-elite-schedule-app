7efa01f91c4f10a156b7f922fa8615023a22c208
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-19 22:03:40 -0400

    Standings & TeamDetails, Add team reference passed into ion-tab

--- src/pages/standings/standings.ts
+++ src/pages/standings/standings.ts
@@ -7,11 +7,14 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class StandingsPage {
 
+	public team:any		= {};
+
   constructor(
 	  public navCtrl: NavController
 	  , public navParams: NavParams
 	) {
 		console.log("navParams:", this.navParams );
+		this.team = this.navParams.data;
   }
 
   ionViewDidLoad() {
--- src/pages/team-detail/team-detail.ts
+++ src/pages/team-detail/team-detail.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { MyTeamsPage } from '../my-teams/my-teams';
 
 @Component({
   selector: 'page-team-detail',
@@ -7,18 +8,31 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class TeamDetailPage {
 
-	// public team:any = {};
+	public team:any = {};
 
   constructor(
 	  public navCtrl: NavController
 	  , public navParams: NavParams
 	) {
-	// this.team = this.navParams.data;//will be an incoming Team instance
+	this.team = this.navParams.data;//will be an incoming Team instance
+	//this is still param 2 when using nav.push() / navCtrl.push()
+	// -OR- when when passed via the [rootParams] binding in <ion-tab> component markup
 	console.log("navParams:", this.navParams );
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TeamDetailPage');
   }
+
+//   goHome(){//for demonstration of nested stack problems
+// 	// this.navCtrl.push( MyTeamsPage );
+// 		// ^^^ will add the "home" page to the internal/parent tabs stack (as a child of the TeamHomePage)
+// 	// this.navCtrl.popToRoot();//same fix as in the parent TeamHomePage
+// 		// ^^^ does nothing bc, in context, this IS root of the tab stack already
+
+// 		this.navCtrl.parent.parent.popToRoot();
+// 		//get ref to ^tabs^ ^root^ ^^^^ go from there
+
+//   }
 
 }
