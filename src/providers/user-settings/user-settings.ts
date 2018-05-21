6cbe79a9fbdb61031c4790586ae99163e55274ff
Todd Mullen <ToddJ.Mullen@Gmail.com>
2018-05-20 20:33:20 -0400

    UserSetting, Implement (un)favoriteTeam() & isFavoriteTeam() methods

--- src/providers/user-settings/user-settings.ts
+++ src/providers/user-settings/user-settings.ts
@@ -1,11 +1,24 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Storage } from "@ionic/storage";
 
 @Injectable()
 export class UserSettings {
 
-  constructor(public http: HttpClient) {
+  constructor(private storage: Storage) {
     console.log('Hello UserSettings Provider');
   }
+
+	favoriteTeam( team, tourId, tourName ){
+		let item = { team, tourId, tourName }
+		this.storage.set( team.id.toString(), JSON.stringify(item) );
+	}
+	unfavoriteTeam( team ){
+	  this.storage.remove( team.id.toString() );
+	}
+
+	isFavoriteTeam( teamId: string ): Promise<boolean>{
+		return this.storage.get( teamId ).then( t => t ? true: false );
+	}
+
 
 }
